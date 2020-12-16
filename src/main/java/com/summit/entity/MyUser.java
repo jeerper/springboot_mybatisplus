89887921bad83e28adaa153a977682cfa6db7238
jeerper <297272453@qq.com>
2020-12-16 15:51:59 +0800

    增加实体类

---
+++ src/main/java/com/summit/entity/MyUser.java
@@ -0,0 +1,47 @@
+package com.summit.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * @author: jeerper
+ * @since: 2020/12/16 15:49
+ */
+public class MyUser  implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
